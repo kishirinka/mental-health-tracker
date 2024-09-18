fce745c5bfc0a3e86d4d4093ae8a376b3f61c95d
kishirinka <irmaalwizah@gmail.com>
2024-09-18 12:44:51 +0700

    ayooww

--- main/views.py
+++ main/views.py
@@ -40,7 +40,6 @@ def show_json(request):
     data = MoodEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-
 def show_xml_by_id(request, id):
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
